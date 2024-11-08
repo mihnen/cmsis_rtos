9e1245446fdacaaac111f6e2750d3363135ea3f9
Matt Ihnen <matt.ihnen@gmail.com>
2024-11-07 22:41:05 -0600

    Expose a bunch of build options

--- build.zig
+++ build.zig
@@ -5,11 +5,113 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const options = b.addOptions();
+
+    const dynamic_mem_size = b.option(u32, "dynamic_mem_size", "Global memory size (must be multiple of 8!)") orelse 32768;
+    const tick_freq = b.option(u32, "tick_freq", "system tick frequency") orelse 1000;
+    const round_robin_to = b.option(u32, "round_robin_to", "round robin timeout (in ticks) before tasks switch") orelse 5;
+    const enable_round_robin = b.option(bool, "enable_round_robin", "Enable round robin task switching") orelse true;
+    const enable_stack_checks = b.option(bool, "enable_stack_checks", "Enable stack overflow checks when tasks switch") orelse false;
+    const enable_stack_watermark = b.option(bool, "enable_stack_watermark", "Enables stack watermark for checking task stack usage") orelse false;
+    const isr_queue_size = b.option(u32, "isr_queue_size", "Size of isr fifo queue") orelse 16;
+
+    options.addOption(u32, "dynamic_mem_size", dynamic_mem_size);
+    options.addOption(u32, "tick_freq", tick_freq);
+    options.addOption(u32, "round_robin_to", round_robin_to);
+    options.addOption(bool, "enable_round_robin", enable_round_robin);
+    options.addOption(bool, "enable_stack_checks", enable_stack_checks);
+    options.addOption(bool, "enable_stack_watermark", enable_stack_watermark);
+    options.addOption(u32, "isr_queue_size", isr_queue_size);
+
     const lib = b.addStaticLibrary(.{
         .name = "cmsis_rtos_clib",
         .target = target,
         .optimize = optimize,
     });
+
+    const dynamic_mem_size_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{dynamic_mem_size},
+    );
+
+    if (dynamic_mem_size_value) |value| {
+        lib.defineCMacro("OS_DYNAMIC_MEM_SIZE", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const tick_freq_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{tick_freq},
+    );
+
+    if (tick_freq_value) |value| {
+        lib.defineCMacro("OS_TICK_FREQ", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const round_robin_to_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{round_robin_to},
+    );
+
+    if (round_robin_to_value) |value| {
+        lib.defineCMacro("OS_ROBIN_TIMEOUT", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const enable_stack_checks_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{@intFromBool(enable_stack_checks)},
+    );
+
+    if (enable_stack_checks_value) |value| {
+        lib.defineCMacro("OS_STACK_CHECK", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const enable_stack_watermark_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{@intFromBool(enable_stack_watermark)},
+    );
+
+    if (enable_stack_watermark_value) |value| {
+        lib.defineCMacro("OS_STACK_WATERMARK", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const isr_queue_size_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{isr_queue_size},
+    );
+
+    if (isr_queue_size_value) |value| {
+        lib.defineCMacro("OS_ISR_FIFO_QUEUE", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
+
+    const enable_round_robin_value = std.fmt.allocPrint(
+        b.allocator,
+        "{d}",
+        .{@intFromBool(enable_round_robin)},
+    );
+
+    if (enable_round_robin_value) |value| {
+        lib.defineCMacro("OS_ROBIN_ENABLE", value);
+    } else |err| {
+        std.debug.print("{s}\n", .{@errorName(err)});
+    }
 
     newlib.addIncludeHeadersAndSystemPathsTo(b, target, lib) catch |err| switch (err) {
         newlib.Error.CompilerNotFound => {
